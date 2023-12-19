ace306d167f14a78d2030bb7773f86e77c3dc108
Sayere1 <osayemwenreosaigbovo@gmail.com>
2023-12-19 15:20:38 -0800

    func

---
+++ print_array.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_array - array of integers to be printed
+ *
+ * @array: array to be printed
+ * @size: Num of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+	size_t i;
+
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
+}
